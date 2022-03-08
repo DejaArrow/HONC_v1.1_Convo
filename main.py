52e6bb32c0b8168bdca07e047b3f31c94dc618ad
DeJaArrow <p110122341@student.peterborough.ac.uk>
2022-03-08 14:57:29 +0000

    Dataset Doubled, max accuracy 91, overfitting

---
+++ main.py
@@ -0,0 +1,120 @@
+import numpy as np
+import os
+import PIL
+import PIL.Image
+import tensorflow as tf
+import pathlib
+import matplotlib.pyplot as plt
+from functools import partial
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+data_dir = pathlib.Path('assets/train')
+
+image_count = len(list(data_dir.glob('*/*.png')))
+print(image_count)
+
+
+img_height = 100
+img_width = 100
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split = 0.2,
+    subset='training',
+    seed=123,
+    image_size =(img_height, img_width),
+    )
+
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    )
+
+class_names = train_ds.class_names
+print(class_names)
+
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+normalization_layer = tf.keras.layers.Rescaling(1./255)
+
+num_classes = len(class_names)
+
+DefaultConv2D = partial(keras.layers.Conv2D,
+                        kernel_size=3, activation='relu', padding="SAME")
+
+model = keras.models.Sequential([
+  DefaultConv2D(filters=64, kernel_size=7, input_shape=[100,100,3]),
+  keras.layers.MaxPooling2D(pool_size=2),
+  DefaultConv2D(filters=128),
+  DefaultConv2D(filters=128),
+  keras.layers.MaxPooling2D(pool_size=2),
+  DefaultConv2D(filters=256),
+  DefaultConv2D(filters=256),
+  keras.layers.MaxPooling2D(pool_size=2),
+  keras.layers.Flatten(),
+  keras.layers.Dense(units=128, activation='relu'),
+  keras.layers.Dropout(0.5),
+  keras.layers.Dense(units=64, activation='relu'),
+  keras.layers.Dropout(0.5),
+  keras.layers.Dense(units=10, activation='softmax'),
+])
+
+model.compile(optimizer='adam', 
+              loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.summary()
+
+epochs=40
+
+history = model.fit(train_ds,epochs=epochs, validation_data = val_ds)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+stitch_14 = ('test_14.png')
+
+test_img = tf.keras.utils.load_img(
+  stitch_14, target_size=(img_height, img_width)
+)
+
+img_array = tf.keras.utils.img_to_array(test_img)
+img_array = tf.expand_dims(img_array, 0)
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image is most likely a {} with a {:.2f} percent confidence"
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
